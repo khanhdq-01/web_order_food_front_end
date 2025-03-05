ff54316a93b5ea8038dafcd8bd269946441db204
Khanhdq <quockhanhvip98@gmail.com>
2025-03-05 17:28:30 +0700

    fix_layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+<template lang="">
+  <!-- navbar -->
+   <NavBar :name=userName />
+  <div class = "home">
+   <span style="text-align:center" > {{userName}} </span>
+  </div>
 </template>
+<script>
+import router from '@/router';
+import NavBar from '@/components/NavBar.vue';
+export default {
+
+  components:{
+    NavBar
+  },
+  data() {
+    return {
+      userName: ''
+    }
+  },
+  mounted(){
+    this.userName = localStorage.getItem('name')
+    if (!this.userName || this.userName == '' || this.userName ==null){
+      router.push({ name: 'login'})
+    }
+  },
+}
+</script>
+<style lang="">
+  
+</style>
